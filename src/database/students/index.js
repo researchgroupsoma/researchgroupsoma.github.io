80d79a8eaccadb9fce4145f33c9818eb30187d11
Gabriel Menezes <gabrielufmscc@gmail.com>
2021-08-09 19:23:26 -0400

    Adicionando outros estudantes

--- src/database/students/index.js
+++ src/database/students/index.js
@@ -1,6 +1,14 @@
 import publications from "@/database/publications"
 
-let _students = ["001_Gabriel_Menezes.json"]
+let _students =
+    [
+        "001_Gabriel_Menezes.json",
+        "002_Karolina_Neves.json",
+        "003_Adriano_Marques.json",
+        "004_Sabia_Belle.json",
+        "005_Matheus_M.json",
+        "006_Celso_Junior.json"
+    ]
 
 export default new class StudentDatabase {
     students = []
